fecf6553340ffbf01f2803bbcdf002a35ae737aa
Crypto-Hamster <arkadiusz.budkowski.dev@gmail.com>
2021-03-25 21:57:02 +0100

    Pierwszy rzut

---
+++ src/main/java/FreqTest.java
@@ -0,0 +1,42 @@
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class FreqTest {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("<--Pi4J--> GPIO Control Example ... started.");
+
+        // create gpio controller
+        final GpioController gpio = GpioFactory.getInstance();
+
+        // provision gpio pin #01 as an output pin and turn on
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26, "MyLED", PinState.HIGH);
+
+
+        // set shutdown state for this pin
+        pin.setShutdownOptions(true, PinState.LOW);
+        for (int i = 0; i < 100; i++) {
+
+            pin.high();
+            System.out.println("--> GPIO state should change --> TRY: " + i);
+
+            Thread.sleep(200);
+
+            // turn off gpio pin #01
+            pin.low();
+            System.out.println("--> GPIO state should change --> TRY: " + i);
+
+            Thread.sleep(200);
+
+
+        }
+        gpio.shutdown();
+
+        System.out.println("Exiting ControlGpioExample");
+    }
+}
+//END SNIPPET: control-gpio-snippet
+
+
